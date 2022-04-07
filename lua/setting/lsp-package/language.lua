d2dec11353c072dd61cdc979479320a903152226
Ru <slzdru@gmail.com>
2022-04-07 12:11:52 +0800

    lspSaga rename

--- lua/setting/lsp-package/language.lua
+++ lua/setting/lsp-package/language.lua
@@ -1,6 +1,8 @@
 -- In lsp attach function
 local opts = { noremap = true, silent = true }
 
+local Lspsaga = require 'lspsaga'
+
 local buf_map = function(bufnr, mode, lhs, rhs, opt)
   vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opt or {
     silent = true,
@@ -14,7 +16,9 @@ local on_attach = function(client, bufnr)
   vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
   vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
   vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  -- vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  vim.cmd("command! LspRename Lspsaga rename")
+  vim.cmd("command! LspsagaHover Lspsaga hover_doc")
   vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
   vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
   vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
@@ -29,6 +33,8 @@ local on_attach = function(client, bufnr)
   buf_map(bufnr, "n", "gd", ":LspDef<CR>")
   buf_map(bufnr, "n", "gr", ":LspRename<CR>")
   buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
+  buf_map(bufnr, "n", "gh", ":LspsagaHover<CR>")
+
   buf_map(bufnr, "n", "K", ":LspHover<CR>")
   buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
   buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
